9d291e72f4771860cfa73ff00e7b0dcf74d4f211
NelysT <139475672+NelysT@users.noreply.github.com>
2023-11-12 21:33:19 +0000

    navbar - Pongo un navbar provisional q nos lleva a los principales link. Lo he fijado. home - Actualizo la visual y creo home.css template - Pongo el script de fontawesome

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
+	const navbarStyle = {
+		position: "fixed",
+		top: 0,
+		left: 0,
+		width: "100%",
+		zIndex: 100
+		
+		
+	  };
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary" style={navbarStyle}>
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<p className="nav_palante"><strong>PA'LANTE</strong></p>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+				
+				<div className="container_navbar_right d-flex">
+					<div className="dropdown">
+						<button className="btn_comunidad dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Comunidad
+						</button>
+						<ul className="dropdown-menu">
+							<li><a className="dropdown-item" href="#">Chat</a></li>
+							<li><a className="dropdown-item" href="#">Eventos</a></li>
+							
+						</ul>
+					</div>
+					<div className="dropdown">
+						<button className="btn_recursos dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Herramientas
+						</button>
+						<ul className="dropdown-menu">
+							<Link to='/lecturas'>
+								<li><a className="dropdown-item" href="#">Lecturas</a></li>
+							</Link>
+							<Link to='/meditaciones'>
+								<li><a className="dropdown-item" href="#">Meditaciones guiadas</a></li>
+							</Link>
+							<Link to='/podcast'>
+								<li><a className="dropdown-item" href="#">Podcast</a></li>
+							</Link>
+													
+						</ul>
+					</div>
+					<div>
+					<Link to="/citas">
+						<button className="appointment_btn">
+							Citas
+						</button>
 					</Link>
 				</div>
+					<div className="login">
+						<Link to='/login'>
+							<button type="button" className="nav_btn">
+							<i className="fa-regular fa-user"></i> Ingresar
+							</button>
+						</Link>
+						
+						
+						
+					</div>
+					
+				</div>	
+				
 			</div>
 		</nav>
 	);
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import imgHome from "../../img/imgHome.jpg";
+import home from "../../img/home.jpg";
+import profesionales from "../../img/profesionales.png";
+import nosotros from "../../img/nosotros.png";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -10,37 +12,168 @@ export const Home = () => {
 		<div className="container_Landing_Page">
 			<div className="row_Landig_Page">
 				<div className="col_juntos">
-					<img className="imgHome" src={imgHome}></img>
+					<img className="imgHome" src={home}></img>
+					<div className="juntos"><strong>Juntos de la mano...</strong></div>
+					<div className="juntos1"><strong>Pa’lante</strong> está soñada para acompañarte.</div>
 				</div>
-				<div className="title_conocenos d-flex">
-							<p className="linea_conocenos">_________</p>
-							<p className="conocenos">CONOCENOS</p>
-						</div>
-				<div className="col_conocenos d-flex">
+				<div className="container_conocenos d-flex">
 					<div className="col_conocenos_right">
-						
+						<div className="title_conocenos d-flex">
+							<p className="linea_conocenos">_____</p>
+							<p className="conocenos">CONÓCENOS</p>
+						</div>
 						<div className="col_contenido_conocenos">
-							<h1 className="h1_contenido_conocenos_title">Esta iniciativa es resultado de nuestra experiencia.</h1>
+							<h1 className="h1_contenido_conocenos_title"><strong>Esta iniciativa es resultado de nuestra experiencia</strong>.</h1>
 							<p className="p_contenido_conocenos">Hemos soñado este proyecto porque también la hemos pasado mal, hemos perdido la esperanza y nos hemos sentido solos.<br></br> 
 Deseamos que muchos encuentren en este espacio, un lugar donde renueven las esperanzas y la alegria para seguir PA´LANTE.<br></br>
 Queremos hacer este camino Juntos porque hemos estado donde estás.</p>
 						</div>
 					</div>
-					
 					<div className="img_conocenos">
-						<img src="https://media.istockphoto.com/id/1368965646/es/foto/chicos-y-chicas-multi%C3%A9tnicos-que-se-toman-selfies-al-aire-libre-con-luz-de-fondo-concepto-de.jpg?s=612x612&w=0&k=20&c=QC9JqaBFDnZZtwutt6bvGFLXmmtbv9e355syXsG39KE="></img>
+						<img className="conocenos_profesionales" src={nosotros}></img>
 					</div>
 				</div>
-				<div className="col_que_hacemos">
-					
+			
+				<div className="container_hacemos d-flex">
+                    <div className="col_hacemos_right">
+                       <div className="title_conocenos d-flex">
+                            <p className="linea_conocenos">_____</p>
+                            <p className="conocenos">QUÉ HACEMOS</p>
+                        </div>
+
+                       <div className="col_contenido_hacemos">
+                            <h1 className="h1_contenido_hacemos_title"><strong>Qué puedes encontrar con nosotros.</strong></h1>
+                            <div className="lista_hacemos d-flex">
+                                <p><i class="fa-solid fa-house"></i></p>
+                                <h3 className="soporte">Soporte familiar</h3>
+                            </div>
+                            <div>
+                                <p className="explicacion_li_hacemos">Acompañamos a las familias en sus dinámicas cotidianas, brindando recursos de afrontamiento.</p>
+                            </div>
+                            <div className="lista_hacemos d-flex">
+                                <p><i class="fa-solid fa-house"></i></p>
+                                <h3 className="soporte">Bienestar psicológico</h3>
+                            </div>
+                            <div>
+                                <p className="explicacion_li_hacemos">Brindamos recursos semanales que favorecen el bienestar psicológico.</p>
+                            </div>
+                            <div className="lista_hacemos d-flex">
+                                <p><i class="fa-solid fa-house"></i></p>
+                                <h3 className="soporte">Comunidad</h3>
+                            </div>
+                            <div>
+                                <p className="explicacion_li_hacemos">Hemos diseñado este espacio para que formes parte de una comunidad que camina junto a ti.</p>
+                            </div>
+                            <div className="lista_hacemos d-flex">
+                                <p><i class="fa-solid fa-house"></i></p>
+                                <h3 className="soporte">Terapia</h3>
+                            </div>
+                            <div>
+                                <p className="explicacion_li_hacemos">Contamos con una familia de profesionales que esperan por ti.</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div>
+					<div className="img_conocenos">
+                        <img className="img_hacemos" src="https://images.pexels.com/photos/10435733/pexels-photo-10435733.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+                    </div>
+
+
+                    
+                </div>
 				</div>
-				<div className="col">
-					
+
+				<div className="col_todo_list">
+					<div className="title_conocenos d-flex">
+						<p className="linea_conocenos">_____</p>
+						<p className="conocenos">PROYECTO TODO-LIST</p>
+					</div>
+					<div className="col_contenido_conocenos">
+							<h1 className="h1_contenido_todolist_title"><strong>Hemos pensado un espacio para tu lista de tareas PA´LANTE.</strong></h1>
+					</div>
+					<div className="container_img_todolist d-flex">
+						<div className="todolist_agradecer">
+							<img className="img_agradecer" src="https://images.pexels.com/photos/9035958/pexels-photo-9035958.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+							<div className="agradecer"><strong>Agradecer</strong></div>
+						</div>
+						<div className="todolist_ejercitarse">
+							<img className="img_ejercitarse" src="https://images.pexels.com/photos/7210602/pexels-photo-7210602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+							<div className="ejercitarse"><strong>Ejercitarse</strong></div>
+						</div>
+						<div className="todolist_sonreir">
+							<img className="img_sonreir" src="https://images.pexels.com/photos/3934003/pexels-photo-3934003.jpeg?auto=compress&cs=tinysrgb&w=600"></img>
+							<div className="sonreir"><strong>Sonreir</strong></div>
+						</div>
+					</div>
 				</div>
-				<div className="col">
-					
+				<div className="container_equipo d-flex">
+					<div className="equipo_right">
+						<div className="title_equipo d-flex">
+								<p className="linea_conocenos">_____</p>
+								<p className="conocenos">NUESTRO EQUIPO</p>
+						</div>
+						<div className="col_equipo d-flex">
+							<div className="col_equipo_right">
+								<div className="col_contenido_equipo">
+									<h1 className="h1_contenido_equipo_title"><strong>Conoce a nuestros Profesionales.</strong></h1>
+									<p className="contenido_equipo">Contamos con un equipo de profesionales y colaboradores especializados en diferentes terapias, que te ayudarán a encontrar una solución, a medida que desarrollas habilidades y rediriges el rumbo de tu vida</p>
+									<p className="colaborar"><strong>¿Te gustaría colaborar?</strong></p>
+									<button className="btn_colaborar">Trabajar como freelancer</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col_equipo_left">
+						<img className="img_equipo" src={profesionales}></img>
+					</div>
 				</div>
-				<div className="col">
+				<div className="col_eventos">
+					<div className="title_eventos">
+						<h1 className="h1_title_eventos">Nuestros eventos</h1>
+					</div>
+					<div className="propuesta_eventos d-flex">
+						<div className="container_evento_1">
+							<div className="evento_1 d-flex">
+								<div className="evento_1_fecha">
+									<p className="dia"><strong>23</strong></p>
+									<p className="mes">Nov</p>
+								</div>
+								<div className="proximo_evento">
+									<div className="arriba_prox_evento d-flex">
+										<p className="prox_evento">PRÓXIMO EVENTO</p>
+										<p className="linea_evento">____</p>
+									</div>
+									
+									<p className="title_prox_evento">Presentación del proyecto Pa'lante</p>
+
+								</div>
+								<div>
+									<p className="ir_evento"><i class="fa-solid fa-circle-right"></i></p>
+								</div>
+							</div>
+						</div>
+						<div className="container_evento_1">
+							<div className="evento_1 d-flex">
+								<div className="evento_1_fecha">
+									<p className="dia"><strong>15</strong></p>
+									<p className="mes">Dic</p>
+								</div>
+								<div className="proximo_evento">
+									<div className="arriba_prox_evento d-flex">
+										<p className="prox_evento">PRÓXIMO EVENTO</p>
+										<p className="linea_evento">____</p>
+									</div>
+									
+									<p className="title_prox_evento">Conferencia con Dr.Manuel Maza</p>
+
+								</div>
+								<div>
+									<p className="ir_evento"><i class="fa-solid fa-circle-right"></i></p>
+								</div>
+							</div>
+						</div>
+					</div>
 					
 				</div>
 
