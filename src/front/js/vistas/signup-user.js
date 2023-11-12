5285f498ba276e7216ff98bf316d78046a8e420a
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-12 16:07:38 +0000

    Incorporo confirmacion de contraseña

--- src/front/js/vistas/signup-user.js
+++ src/front/js/vistas/signup-user.js
@@ -9,16 +9,22 @@ export const Signup_user = () => {
   const navigate = useNavigate();
   const [register, setRegister] = useState({ full_name: '', email: '', password: '' })
   const [submit, setSubmit] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault() //Evitar el comportamiento predeterminado que normalmente ocurre cuando se produce un evento. 
+
+    // Condición para verificar que las contraseñas coincidan
+    if (register.password !== confirmPassword) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
 
     // Condiciona al usuario para completar los campos 
     if (register.full_name.trim() === "" || register.email.trim() === "" || register.password.trim() === "") {
       alert("Los campos son requeridos");
       return;
     }
-
     // Se ejecuta el fetch desde flux para almacenar los datos del usuario en la base de datos 
     actions.signupUser(register);
     setSubmit(true);
@@ -44,9 +50,10 @@ export const Signup_user = () => {
         </div>
         <form className="form" onSubmit={handleSubmit}>
           <div className="container-inputs">
-            <input type="name" className="form-control" name="full name" id="exampleInputEmail1" placeholder="Nombre Completo" aria-describedby="emailHelp" value={register.full_name} onChange={(e) => setRegister({ ...register, full_name: e.target.value })} />
+            <input type="name" className="form-control" name="full name" id="exampleInputName1" placeholder="Nombre Completo" aria-describedby="emailHelp" value={register.full_name} onChange={(e) => setRegister({ ...register, full_name: e.target.value })} />
             <input type="email" className="form-control" name="email" id="exampleInputEmail1" placeholder="Correo Electrónico" aria-describedby="emailHelp" value={register.email} onChange={(e) => setRegister({ ...register, email: e.target.value })} />
-            <input type="Password" className="form-control" name="Password" id="exampleInputEmail1" placeholder="Contraseña" aria-describedby="emailHelp" value={register.password} onChange={(e) => setRegister({ ...register, password: e.target.value })} />
+            <input type="password" className="form-control" name="Password" id="exampleInputPassword1" placeholder="Contraseña" aria-describedby="emailHelp" value={register.password} onChange={(e) => setRegister({ ...register, password: e.target.value })} />
+            <input type="password" className="form-control" name="Confirm Password" id="exampleInputPassword2" placeholder="Repite Contraseña" aria-describedby="emailHelp" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
           </div>
           <button className="login">Registrate</button>
         </form>
