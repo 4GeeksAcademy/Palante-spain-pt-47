fb8399d1a6190c7463fa39d07973e83096182836
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-13 23:52:34 +0000

    Falta revisar fallo flux

--- src/front/js/vistas/signup-freelance.js
+++ src/front/js/vistas/signup-freelance.js
@@ -1,24 +1,192 @@
-import React, { useState, useEffect, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import fondo from "/workspaces/spain_part_time47/src/front/img/imghomeazul__1_.png"
+import { Link } from "react-router-dom";
+import { Formik } from "formik";
 
-export const Signup_freelancer = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+export const Signup_freelancer = () => {
+  const { actions } = useContext(Context);
+  const navigate = useNavigate();
+  const [register, setRegister] = useState({ full_name: '', email: [''], password: '', confirm_password: '', age: '', years_of_experience: '' })
+  const [submit, setSubmit] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element:</h1>
-			<img src={rigoImageUrl} />
-			<hr className="my-4" />
+  const handleSubmit = (e) => {
+    e.preventDefault() //Evitar el comportamiento predeterminado que normalmente ocurre cuando se produce un evento. 
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
-};
+
+    // Se ejecuta el fetch desde flux para almacenar los datos del usuario en la base de datos 
+    actions.signupFreelancer(register);
+    setSubmit(true)
+    setTimeout(() => {
+      setSubmit(false);
+      navigate("/login");
+    }, 1000);
+
+    setRegister('')
+  }
+
+  return (
+    <div className="container-fluid">
+      <img className="imagen-fondo" src={fondo} />
+      <div className="container-form">
+        <div className="container-logo">
+          <h1 className="logo">Pa'lante</h1>
+        </div>
+        <Formik
+          validate={() => {
+            let errores = {}
+
+            //Validacion Nombre
+            if (register.full_name == "") {
+              errores.full_name = "Por favor, ingresa un nombre completo"
+            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(register.full_name)) {
+              errores.full_name = "El campo 'Nombre Completo' acepta letras y espacios"
+            }
+
+            //Validacion Correo
+            if (register.email == "") {
+              errores.email = "Por favor, ingresa un correo electrónico"
+            } else if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(register.email)) {
+              errores.email = "El campo 'Correo Electrónico' acepta letras, numeros, puntos y guiones "
+            }
+
+            //Validacion Contraseña
+            if (register.password == "") {
+              errores.password = "Por favor, ingresa una contraseña"
+            } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,15}$/.test(register.password)) {
+              errores.password = "La contraseña acepta entre 8-15 caracteres, al menos una letra mayúscula y una minúscula, dígitos y caracteres especiales"
+            }
+
+            //Confirmacion de Contraseña
+            if (confirmPassword == "") {
+              errores.confirm_password = "Por favor, rellena el campo faltante"
+            } else if (register.password.length >= 8) {
+              if (register.password !== confirmPassword) {
+                errores.confirm_password = "Las contraseñas no coinciden"
+                return;
+              } else {
+                console.log("Las contraseñas coinciden");
+              }
+            }
+
+             //Validacion Edad
+             if (register.age == "") {
+              errores.age = "Por favor, ingrese su edad"
+            } else if (!/^(1[89]|[2-9]\d|\d{3,})$/.test(register.age)) {
+              errores.age = "Debe ser mayor de 18 para registrarse"
+            }
+
+            //Validacion de Experiencia
+            if (register.years_of_experience == "") {
+              errores.years_of_experience = "Por favor, seleccione una opcion"
+            } 
+
+            return errores;
+          }}
+        >
+          {({ handleBlur, errors, touched }) => (
+            <form className="form-inputs" onSubmit={handleSubmit}>
+              <div className="container-inputs">
+                <div className="detalle-input">
+                  <input
+                    type="name"
+                    className="form-control"
+                    name="full_name"
+                    id="exampleInputName1"
+                    placeholder="Nombre Completo"
+                    aria-describedby="emailHelp"
+                    value={register.full_name}
+                    onChange={(e) => setRegister({ ...register, full_name: e.target.value })}
+                    onBlur={handleBlur}
+                  />
+                  {touched.full_name && errors.full_name && <div className="error">{errors.full_name}</div>}
+                </div>
+                <div className="detalle-input">
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="exampleInputEmail1"
+                    placeholder="Correo Electrónico"
+                    aria-describedby="emailHelp"
+                    value={register.email}
+                    onChange={(e) => setRegister({ ...register, email: e.target.value })}
+                    onBlur={handleBlur}
+                  />
+                  {touched.email && errors.email && <div className="error">{errors.email}</div>}
+                </div>
+                <div className="detalle-input">
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    id="exampleInputPassword1"
+                    placeholder="Contraseña"
+                    aria-describedby="emailHelp"
+                    value={register.password}
+                    onChange={(e) => setRegister({ ...register, password: e.target.value })}
+                    onBlur={handleBlur}
+                  />
+                  {touched.password && errors.password && <div className="error">{errors.password}</div>}
+                </div>
+                <div className="detalle-input">
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="confirm_password"
+                    id="exampleInputPassword2"
+                    placeholder="Repetir Contraseña"
+                    aria-describedby="emailHelp"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    onBlur={handleBlur}
+                  />
+                  {touched.confirm_password && errors.confirm_password && <div className="error">{errors.confirm_password}</div>}
+                </div>
+                <div className="detalle-input">
+                  <input
+                    type="number"
+                    className="form-control"
+                    name="age"
+                    id="exampleInputAge1"
+                    placeholder="Edad"
+                    aria-describedby="emailHelp"
+                    value={register.age}
+                    onChange={(e) => setRegister({ ...register, age: e.target.value })}
+                    onBlur={handleBlur}
+                  />
+                  {touched.age && errors.age && <div className="error">{errors.age}</div>}
+                </div>
+                <div className="detalle-input">
+                  <select
+                    className="select"
+                    name="years_of_experience"
+                    id="exampleInputExperience1"
+                    placeholder="Años de Experiencia"
+                    aria-describedby="emailHelp"
+                    value={register.years_of_experience}
+                    onChange={(e) => setRegister({...register, years_of_experience: e.target.value})}
+                    onBlur={handleBlur}
+                  >
+                    <option value="" disabled hidden>Selecciona una opcion</option>
+                    <option value="0-2">0-2 años</option>
+                    <option value="2-5">2-5 años</option>
+                    <option value="5-10">5-10 años</option>
+                    <option value="mas-de-10">Más de 10 años</option>
+                  </select>
+                  {touched.years_of_experience && errors.years_of_experience && <div className="error">{errors.years_of_experience}</div>}
+                </div>
+
+              </div>
+              <button className="boton-registro">Registrate</button>
+              {submit && <p className="exito">Te haz registrado con exito</p>}
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+
+}
