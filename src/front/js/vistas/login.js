370b45c2d03f36653d95396b94c2b20016acbe7b
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-13 11:42:18 +0000

    Importacion de imagen de fondo y ajuste en los contenedores

--- src/front/js/vistas/login.js
+++ src/front/js/vistas/login.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import fondo from "/workspaces/spain_part_time47/src/front/img/imghomeazul__1_.png"
 
 export const Login = () => {
 
@@ -39,25 +40,24 @@ export const Login = () => {
 
   return (
     <div className="container-fluid">
+      <img className="imagen-fondo" src={fondo} />
       <div className="container-form">
         <div className="container-logo">
           <h1 className="logo">Pa'lante</h1>
         </div>
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form-inputs" onSubmit={handleSubmit}>
           <div className="container-inputs">
             <input type="email" className="form-control" name="email" id="exampleInputEmail1" placeholder="Correo Electrónico" aria-describedby="emailHelp" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
             <input type="Password" className="form-control" name="Password" id="exampleInputPassword1" placeholder="Contraseña" aria-describedby="emailHelp" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
           </div>
-          <button className="login">Inicia sesion</button>
-        </form>
-        <div className="opciones">
+          <button className="boton-login">Inicia sesion</button>
           <Link to=""> 
             <p className="opcion-contraseña">¿Haz olvidado tu contraseña?</p>
           </Link>
-          <Link to="">
-            <p className="opcion-registro">¿Aún no tienes cuenta? Registrate</p>
-          </Link>
-        </div>
+        </form>
+        <Link to="" className="ruta-register">
+          <p className="opcion-registro">¿Aún no tienes cuenta? Registrate</p>
+        </Link>
       </div>
     </div>
   )
