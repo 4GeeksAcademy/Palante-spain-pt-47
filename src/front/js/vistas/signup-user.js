ae992e75f1e7177f843447b30db897948c50e38d
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-13 22:49:10 +0000

    Esta en funcionamiento el registro de usuario

--- src/front/js/vistas/signup-user.js
+++ src/front/js/vistas/signup-user.js
@@ -1,36 +1,33 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-
-
+import fondo from "/workspaces/spain_part_time47/src/front/img/imghomeazul__1_.png"
+import { Link } from "react-router-dom";
+import { Formik, Field, Form } from "formik";
 
 export const Signup_user = () => {
   const { actions } = useContext(Context);
   const navigate = useNavigate();
-  const [register, setRegister] = useState({ full_name: '', email: '', password: '' })
+  const [register, setRegister] = useState({ full_name: '', email: [''], password: '', confirm_password: '' })
   const [submit, setSubmit] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault() //Evitar el comportamiento predeterminado que normalmente ocurre cuando se produce un evento. 
 
-    // Condición para verificar que las contraseñas coincidan
-    if (register.password !== confirmPassword) {
-      alert("Las contraseñas no coinciden");
-      return;
-    }
-
-    // Condiciona al usuario para completar los campos 
-    if (register.full_name.trim() === "" || register.email.trim() === "" || register.password.trim() === "") {
-      alert("Los campos son requeridos");
-      return;
-    }
+    
     // Se ejecuta el fetch desde flux para almacenar los datos del usuario en la base de datos 
     actions.signupUser(register);
-    setSubmit(true);
+    setSubmit(true)
+    setTimeout(() => {
+      setSubmit(false);
+      navigate("/login");
+    }, 1000);
+
+    setRegister('')
   }
   // Si submit es true, muestra un mensaje de confirmación y redirige a login
-  if (submit) {
+  /*if (submit) {
     setTimeout(() => {
       navigate("/login"); // Navigate ejecuta la redireccion a login
     }, 800);
@@ -40,23 +37,125 @@ export const Signup_user = () => {
         <p className="alert alert-success-emphasis">Usuario creado con exito.</p>
       </div>
     );
-  };
+    
+  };*/
 
   return (
     <div className="container-fluid">
+      <img className="imagen-fondo" src={fondo} />
       <div className="container-form">
         <div className="container-logo">
           <h1 className="logo">Pa'lante</h1>
         </div>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="container-inputs">
-            <input type="name" className="form-control" name="full name" id="exampleInputName1" placeholder="Nombre Completo" aria-describedby="emailHelp" value={register.full_name} onChange={(e) => setRegister({ ...register, full_name: e.target.value })} />
-            <input type="email" className="form-control" name="email" id="exampleInputEmail1" placeholder="Correo Electrónico" aria-describedby="emailHelp" value={register.email} onChange={(e) => setRegister({ ...register, email: e.target.value })} />
-            <input type="password" className="form-control" name="Password" id="exampleInputPassword1" placeholder="Contraseña" aria-describedby="emailHelp" value={register.password} onChange={(e) => setRegister({ ...register, password: e.target.value })} />
-            <input type="password" className="form-control" name="Confirm Password" id="exampleInputPassword2" placeholder="Repite Contraseña" aria-describedby="emailHelp" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-          </div>
-          <button className="login">Registrate</button>
-        </form>
+        <Formik
+        validate={()=>{
+          let errores = {}
+
+          //Validacion Nombre
+          if(register.full_name == ""){
+            errores.full_name = "Por favor, ingresa un nombre completo"
+          }else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(register.full_name)){
+            errores.full_name = "El campo 'Nombre Completo' acepta letras y espacios"
+          }
+          
+          //Validacion Correo
+          if(register.email == ""){
+            errores.email = "Por favor, ingresa un correo electrónico"
+          }else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(register.email)){
+            errores.email = "El campo 'Correo Electrónico' acepta letras, numeros, puntos y guiones "
+          }
+
+          //Validacion Contraseña
+          if(register.password == ""){
+            errores.password = "Por favor, ingresa una contraseña"
+          }else if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,15}$/.test(register.password)){
+            errores.password = "La contraseña acepta entre 8-15 caracteres, al menos una letra mayúscula y una minúscula, dígitos y caracteres especiales"
+          }
+
+          //Confirmacion de Contraseña
+          if(confirmPassword == ""){
+            errores.confirm_password = "Por favor, rellena el campo faltante"
+          }else if(register.password.length >= 8){
+            if (register.password !== confirmPassword) {
+              errores.confirm_password = "Las contraseñas no coinciden"
+              return;
+            } else {
+              console.log("Las contraseñas coinciden");
+            }
+          }
+          
+          return errores;
+        }}
+        >
+          {( {handleBlur, errors, touched} ) => (
+            <form className="form-inputs" onSubmit={handleSubmit}>
+              <div className="container-inputs">
+                <div className="detalle-input">
+                  <input 
+                    type="name"
+                    className="form-control"
+                    name="full_name"
+                    id="exampleInputName1"
+                    placeholder="Nombre Completo"
+                    aria-describedby="emailHelp"
+                    value={register.full_name}
+                    onChange={(e) => setRegister({ ...register, full_name: e.target.value })}
+                    onBlur={handleBlur} 
+                  />
+                  {touched.full_name && errors.full_name &&  <div className="error">{errors.full_name}</div>}
+                </div>
+                <div className="detalle-input">
+                  <input 
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="exampleInputEmail1"
+                    placeholder="Correo Electrónico"
+                    aria-describedby="emailHelp"
+                    value={register.email}
+                    onChange={(e) => setRegister({ ...register, email: e.target.value })}
+                    onBlur={handleBlur} 
+                  />
+                  { touched.email && errors.email && <div className="error">{errors.email}</div>}
+                </div>  
+                <div className="detalle-input">
+                  <input 
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    id="exampleInputPassword1"
+                    placeholder="Contraseña"
+                    aria-describedby="emailHelp"
+                    value={register.password}
+                    onChange={(e) => setRegister({ ...register, password: e.target.value })}
+                    onBlur={handleBlur} 
+                  />
+                  {touched.password && errors.password && <div className="error">{errors.password}</div>}
+                </div>
+                <div className="detalle-input">
+                  <input 
+                    type="password"
+                    className="form-control"
+                    name="confirm_password"
+                    id="exampleInputPassword2"
+                    placeholder="Repetir Contraseña"
+                    aria-describedby="emailHelp"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    onBlur={handleBlur} 
+                  />
+                  {touched.confirm_password && errors.confirm_password && <div className="error">{errors.confirm_password}</div>}
+                </div>
+                
+              </div>
+              <button className="boton-login">Registrate</button>
+              {submit && <p className="exito">Te haz registrado con exito</p>}
+            </form>
+          )}
+        </Formik>
+        <Link to="/signup-freelance" className="ruta-freelance">
+          <p className="opcion-registro">¿Te gustaría colaborar?</p>
+        </Link>
       </div>
     </div>
   );
