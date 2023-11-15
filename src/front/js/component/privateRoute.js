50dc96dbd318577e6f5fac7bf124df463bae5593
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-15 20:52:24 +0000

    Componente para restringir acceso a vistas

---
+++ src/front/js/component/privateRoute.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Route, Redirect } from 'react-router-dom';
+
+export const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+    }
+  />
+);
