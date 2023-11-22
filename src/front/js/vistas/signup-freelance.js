18f688729774086291604cbedf7feed53aac7ff5
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-22 21:28:41 +0000

    Correccion enlace foto

--- src/front/js/vistas/signup-freelance.js
+++ src/front/js/vistas/signup-freelance.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
-import fondo from "/workspaces/spain_part_time47/src/front/img/imghomeazul__1_.png"
+import fondo from "../../img/imghomeazul__1_.png"
 import { Formik } from "formik";
 
 export const Signup_freelancer = () => {
