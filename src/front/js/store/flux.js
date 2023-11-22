e6c685718e260893d155f570a62aba0833a8fcbc
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-22 10:27:09 +0000

    Solucion datauser

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,6 +2,7 @@ const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
       message: null,
+      datauser: [{}],
       readings: [],
       podcast: [],
       meditations: [],
