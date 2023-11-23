59c572ec54c3ea525b72d0fe968e7ffec9385f7d
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-23 15:22:04 +0000

    Estructura basica todo-list, se puede añadir tarea y mostrar las que hay

--- src/front/js/vistas/todo-list.js
+++ src/front/js/vistas/todo-list.js
@@ -1,12 +1,97 @@
 import React, { useEffect, useState } from "react";
 
 export const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  console.log("TASKS", tasks);
+  console.log("INPUT", input);
+
+  // Traer las tareas
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+
+    fetch(process.env.BACKEND_URL + '/tasks', {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": 'Bearer ' + token
+      },
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        setTasks(data);
+      })
+      .catch(err => console.log("err", err))
+  }, []);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (selectedTask) {
+      // Modificar tarea existente
+      const token = sessionStorage.getItem('token');
+      fetch(process.env.BACKEND_URL + `/updatetask/${selectedTask.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ tasks: input }),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": 'Bearer ' + token
+        },
+      })
+        .then(resp => resp.json())
+        .then(data => {
+          console.log("TAREA MODIFICADA", data);
+          setTasks(tasks.map(task => (task.id === selectedTask.id ? data : task)));
+          setInput(""); // Limpiar el input después de modificar la tarea
+          setSelectedTask(null); // Desmarcar la tarea seleccionada
+        })
+        .catch(err => console.log("err", err))
+    } else {
+      // Agregar nueva tarea
+      if (input.trim() === "") {
+        // Evitar agregar una tarea vacía o solo con espacios en blanco
+        return;
+      }
+
+      const token = sessionStorage.getItem('token');
+      fetch(process.env.BACKEND_URL + '/createtask', {
+        method: "POST",
+        body: JSON.stringify({ tasks: input }),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": 'Bearer ' + token
+        },
+      })
+        .then(resp => resp.json())
+        .then(data => {
+          console.log("TAREA CREADA", data);
+          setTasks(prevTasks => [...prevTasks, data]);
+          setInput(""); // Limpiar el input después de agregar la tarea
+        })
+        .catch(err => console.log("err", err))
+    }
+  };
 
 
-  return(
+
+
+  return (
     <>
-    <input type="text" placeholder="Añade tus tareas" />
-    <button>Añadir</button>
+      <form onSubmit={handleClick}>
+        <input type="text" placeholder="Añade tus tareas" onChange={(e) => setInput(e.target.value)} value={input} />
+        <button type="submit"><i className="fas fa-plus"></i></button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.tasks}
+            <button ><i className="fas fa-pen-to-square"></i></button>
+            <button ><i className="fas fa-trash"></i></button>
+          </li>
+        ))}
+      </ul>  
     </>
   )
 }
