6987ab17dbfbbe1d09c714e68936e8e056610572
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-23 11:22:09 +0000

    Estructura basica para la recuperacion de contraseña

--- src/front/js/vistas/recover-password.js
+++ src/front/js/vistas/recover-password.js
@@ -1,8 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const RecoverPassword = () => {
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Enviar la solicitud de recuperación de contraseña al servidor
+      const response = await fetch('https://tu-servidor/recuperar-contrasena', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        // La solicitud fue exitosa, puedes redirigir al usuario a una página de confirmación
+        console.log('Solicitud de recuperación de contraseña enviada con éxito');
+      } else {
+        // La solicitud falló, maneja el error según sea necesario
+        console.error('Error al enviar la solicitud de recuperación de contraseña');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
+  };
 
   return (
-    <h1>RECUPERAR CONTRASEÑA</h1>
-  )
+    <>
+    <h1>Olvide la contraseña</h1>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Correo Electrónico:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <button type="submit">Enviar solicitud</button>
+    </form>
+    </>
+    
+  );
 }
