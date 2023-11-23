ce9eb7decbfd91c29f0fa588b4e827ece2450b3c
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-23 11:20:46 +0000

    Incorporo columna a la tabla User para almacenar tokens

--- src/api/models.py
+++ src/api/models.py
@@ -11,6 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     URLphoto = db.Column(db.String(200), unique=False, nullable=True)
+    reset_token = db.Column(db.String(100), unique=True, nullable=True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
